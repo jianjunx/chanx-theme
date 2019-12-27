eb842d96570685b69e2ec51baef77cbcd6ff00a7
jayken.xie <jaykenxie@outlook.com>
2019-12-27 17:55:19 +0800

    init

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+hljs.initHighlightingOnLoad();
+(function() {
+  var userInfoDrop = document.querySelector(".user-info-drop");
+  var userInfoTitle = document.querySelector(".user-info_username");
+  userInfoTitle &&
+    userInfoTitle.addEventListener("mouseover", function() {
+      userInfoDrop.style.display = "block";
+    });
+  userInfoTitle &&
+    document.body.addEventListener("click", function(evet) {
+      if (!userInfoDrop.contains(evet.target)) {
+        userInfoDrop.style.display = "none";
+      }
+    });
+  // 实现图片预览
+  var modal = document.querySelector(".modal");
+  var modalImg = document.querySelector(".modal-box img");
+  var postContent = document.querySelector(".post-content");
+  postContent &&
+    postContent.addEventListener("click", function(evet) {
+      if (evet.target.tagName === "IMG") {
+        modalImg.src = evet.target.src;
+        modal.style.display = "block";
+        modalImg.parentElement.style.width = modalImg.clientWidth + "px";
+      }
+    });
+  modal.addEventListener("click", function(event) {
+    if (event.target.tagName === "IMG") return;
+    modal.style.display = "none";
+  });
+  // 友情链接跳转
+  var links = document.querySelector('.links')
+  links && links.addEventListener('click', function(evet){
+    if (evet.target.tagName === "IMG") {
+      window.open(evet.target.alt)
+    }
+  })
+})();
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const { src, dest, parallel, watch } = require('gulp')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const csso = require('gulp-csso')
+const imagemin = require('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
+const sass = require('gulp-sass')
+
+sass.compiler = require('node-sass')
+
+const { MODE } = process.env
+
+// 路径
+function resolve(p) {
+  const _p = MODE == 'dev' ? '../chanx' : 'dist'
+  return path.resolve(__dirname, _p, p)
+}
+function css() {
+  return (
+    src([
+      './views/assets/styles/index.scss',
+      './views/assets/styles/markdown.scss'
+    ])
+      .pipe(sass().on('error', sass.logError))
+      // .pipe(concat('app.min.css'))
+      .pipe(minifyCSS())
+      .pipe(dest('public/css'))
+  )
+}
+function codeStyle() {
+  return src('assets/code-style/*.css')
+    .pipe(csso())
+    .pipe(dest(resolve('assets/code-style/')))
+}
+function js() {
+  return src('assets/js/*.js')
+    .pipe(uglify())
+    .pipe(dest(resolve('assets/js/')))
+}
+
+function img() {
+  return src('assets/img/*')
+    .pipe(imagemin())
+    .pipe(dest(resolve('assets/img/')))
+}
+const htmlminOption = {
+  removeComments: true, //清除HTML注释
+  collapseWhitespace: true, //压缩HTML
+  collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+  removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+  removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+  removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+  minifyJS: true, //压缩页面JS
+  minifyCSS: true //压缩页面CSS
+}
+function php() {
+  return (
+    src('./*.php')
+      // .pipe(htmlmin(htmlminOption))
+      .pipe(dest(resolve('')))
+  )
+}
+function copy() {
+  return src(['./README.md', './screenshot.png']).pipe(dest(resolve('')))
+}
+
+if (MODE === 'dev') {
+  watch('./assets/css/*.scss', css)
+  watch('./assets/js/*.js', js)
+  watch('./*.php', php)
+}
+exports.default = parallel(css, codeStyle, js, img, php, copy)
